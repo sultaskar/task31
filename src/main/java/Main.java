d482bb6b42bdc52c351e7447da0a561fda7e4586
Askar <askarsm93@gmail.com>
2020-08-21 19:06:21 +0300

    Second Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,18 +4,17 @@ import java.util.*;
     public class Main {
 
         public static void main(String[] args) {
-            String number = "21212";
-            checkForPalindrome(number);
+            String string = "madam";
+            checkForPalindrome(string);
+            anotherCheckForPalindrome(string);
         }
-
-
 
 
         public static void checkForPalindrome(String number) {
             int n = number.length();
             int passed = 0;
             int equalSymbols = 0;
-            for (int i = 0; i <= n/2; i++) {
+            for (int i = 0; i <= n / 2; i++) {
                 if (number.charAt(i) == number.charAt((n - 1) - passed)) {
                     equalSymbols++;
                     passed++;
@@ -23,12 +22,23 @@ import java.util.*;
                     passed++;
                 }
             }
-                if (equalSymbols >= n/2){
-                    System.out.println("палиндром");
-                }
-                else {
-                    System.out.println("не палиндром");
-                }
+            if (equalSymbols >= n / 2) {
+                System.out.println("палиндром");
+            } else {
+                System.out.println("не палиндром");
             }
         }
+
+        public static void anotherCheckForPalindrome(String string) {
+
+            StringBuilder stringBuilder = new StringBuilder(string).reverse();
+            String reverseString = stringBuilder.toString();
+            if (string.equals(reverseString)) {
+                System.out.println("палиндром");
+            } else {
+                System.out.println("не палиндром");
+            }
+
+        }
+    }
 
